636858cd5d0270689fb1107fa37a74bd2d5691d7
harshilpethani8990 <76951476+harshilpethani8990@users.noreply.github.com>
2022-06-12 23:26:49 +0530

    Mail Functionality added

--- Routes/sendmsg.js
+++ Routes/sendmsg.js
@@ -1,18 +1,51 @@
 const User = require('../models/user');
+const nodemailer = require("nodemailer");
 
 const router = require('express').Router();
 
-router.post('/', async (req,res)=>{
+
+const sendMail = (msg) => {
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'harshil.s.pethani9957@gmail.com',
+            pass: 'rwezrjhncczlrrpu'
+        }
+    });
+
+    var mailOptions = {
+        from: 'harshil.s.pethani9957@gmail.com',
+        to: 'harshilpethani8990@gmail.com',
+        subject: 'Response From Portfolio Website',
+        text: `${msg}`
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Email Sent");
+        }
+    })
+}
+
+router.post('/', async (req, res) => {
+    sendMail(req.body);
     const msg = new User(req.body);
-    try{
+    try {
         const savedMsg = await msg.save();
 
         res.status(200).send(savedMsg);
 
-    }catch(e){
+    } catch (e) {
         console.log(e);
         res.status(500).send(e);
     }
-})
+});
+
+
+
+
+
 
 module.exports = router;
